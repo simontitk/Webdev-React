924d31fc68bd4f292796776ebbb02f911a3fe472
simontitk <simontitk@gmail.com>
2024-04-29 00:27:59 +0200

    add general footer component

---
+++ src/components/_common/Footer.tsx
@@ -0,0 +1,30 @@
+export default function Footer() {
+
+    return (
+        <>
+            <div className="footer-container">
+                <div className="footer-column-container">
+                    <div className="footer-column">
+                        <h1>Contact</h1>
+                        <ul>
+                            <li>Phone number: +45 12 34 56 78</li>
+                            <li>Email: hydrohomies@hh.dk</li>
+                            <li>Address: ITU Room 4A46 </li>
+                        </ul>
+                    </div>
+                    <div className="footer-column">
+                        <h1>Follow us</h1>
+                        <ul>
+                            <li>Reddit: <span><a href="https://www.reddit.com/r/HydroHomies/">r/HydroHomies</a> </span></li>
+                            <li>Instagram: <span><a href="https://www.instagram.com/hydrohomies/">@HydroHomies</a></span></li>
+                            <li>X: <span><a href="https://twitter.com/hydro_homies">@hydro_homies</a></span></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="footer-description">
+                    Quench your thirst for excellence with our premium selection of artisanal waters, meticulously sourced from the purest natural springs around the globe. Elevate your hydration experience with our handcrafted blends, each bottle a testament to our commitment to purity and quality. From crisp, refreshing still waters, through effervescent delights bursting with natural flavors, to invaluable samples of famous gamer girls' slightly used bath water, every sip embodies the essence of vitality and rejuvenation. Dive into a world of hydration with <strong>HydroHomies</strong>!
+                </div>
+            </div>
+        </>
+    );
+}
