3674162172d44ca558075b3eeca2db04d6fd61a4
simontitk <simontitk@gmail.com>
2024-04-29 00:22:18 +0200

    add template for cart page

---
+++ src/components/cart/CartPage.tsx
@@ -0,0 +1,28 @@
+import "./cart.css";
+
+export default function CartPage() {
+
+    /**
+     * useState declarations
+     */
+
+    /**
+     * helper function declarations
+     */
+
+    /**
+     * fetching within useEffects
+     */
+
+
+    /**
+     * return jsx
+     */
+
+
+    return (
+        <>
+        </>
+
+    );
+}
