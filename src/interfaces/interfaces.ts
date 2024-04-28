bdf1ff45222b93e6263b9a2daba0b250f397f79e
simontitk <simontitk@gmail.com>
2024-04-28 14:03:01 +0200

    create interfaces

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,60 @@
+
+export interface Category {
+    id: number,
+    name: string,
+    description: string
+}
+
+
+export interface Product {
+    id: number,
+    name: string,
+    brand: string,
+    description: string,
+    picture_uri: string,
+    volume: number,
+    amount: number,
+    rating: number,
+    price: number,
+    discounted: boolean,
+    categories?: Category[] 
+    // API does not return categories for products within orders
+}
+
+
+export interface User {
+    id: number,
+    first_name: string,
+    last_name: string,
+    email: string,
+    phone: string,
+    city: string,
+    street: string,
+    password: string,
+    payment_method: string
+}
+
+
+export interface OrderProducts {
+    oid: number,
+    pid: number,
+    quantity: number,
+    product: Product
+}
+
+
+export interface Order {
+    id: number,
+    total_price: number,
+    order_date: string,
+    uid: number,
+    user: User,
+    prouducts: OrderProducts[]
+}
+
+
+export interface CartItem {
+    uid: number,
+    pid: number,
+    quantity: number
+}
