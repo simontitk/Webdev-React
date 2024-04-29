964e843e010fc33795e750a6f2625c610ba300a4
bence <benceluzsinszky@gmail.com>
2024-04-29 16:43:49 +0200

    add product details

--- src/components/single_product/SingleProductPage.tsx
+++ src/components/single_product/SingleProductPage.tsx
@@ -2,24 +2,64 @@ import { useEffect, useState } from "react";
 
 import "./single_product.css";
 
-import { Product } from "../../interfaces/interfaces";
+import { Category, Product, User } from "../../interfaces/interfaces";
 
 import ProductCard from "../_common/ProductCard";
 
 export default function SingleProductPage() {
 
     const [productId, setProductId] = useState(24);
-    const [product, setProduct] = useState<Product[]>([]);
 
-    /**
-     * helper function declarations
-     */
+    const initialProduct: Product = {
+        name: "",
+        id: productId,
+        brand: "",
+        description: "",
+        picture_uri: "",
+        volume: 0,
+        amount: 0,
+        rating: 0,
+        price: 0,
+        discounted: false,
+        categories: []
+    };
+
+    const initialUser: User = {
+        id: 1,
+        first_name: "",
+        last_name: "",
+        email: "",
+        phone: "",
+        city: "",
+        street: "",
+        password: "",
+        payment_method: ""
+    }
+
+    const [product, setProduct] = useState<Product>(initialProduct);
+    const [user, setUser] = useState<User>(initialUser);
+
+    function fillRating(): JSX.Element[] {
+        let droplets = [];
+        for (let i = 1; i <= product.rating; i++) {
+            droplets.push(<img key={i} id={`rating${i}`} src="public/icons/droplet.png" alt="droplet" />);
+        }
+        return droplets;
+    }
 
 
     useEffect(() => {
-        fetch(`http://localhost:3000/${productId}/`)
+        fetch(`http://localhost:3000/products/${productId}/`)
             .then(respone => respone.json())
-            .then((data: Product[]) => { setProduct(data) })
+            .then((data: Product) => { setProduct(data) })
+            .catch(err => console.log(err));
+    }, []);
+
+    const userId: number = 1;
+    useEffect(() => {
+        fetch(`http://localhost:3000/users/${userId}/`)
+            .then(respone => respone.json())
+            .then((data: User) => { setUser(data) })
             .catch(err => console.log(err));
     }, []);
 
@@ -32,36 +72,28 @@ export default function SingleProductPage() {
                 <div className="product">
                     <div className="pictures-rating">
                         <div className="product-img-container">
-                            <img alt="product picture" className="product-image" id="picture" />
+                            <img alt="product picture" className="product-image" id="picture" src={`http://localhost:3000/images/products/${product.picture_uri}`} />
                         </div>
                         <div className="rating">
                             <p>Rating</p>
                             <div className="droplets">
-                                <img id="rating1" alt="droplet" />
-                                <img id="rating2" alt="droplet" />
-                                <img id="rating3" alt="droplet" />
-                                <img id="rating4" alt="droplet" />
-                                <img id="rating5" alt="droplet" />
+                                {product && fillRating()}
                             </div>
                         </div>
                     </div>
                     <div className="product-details">
-                        <p className="single-product-name" id="name"></p>
+                        <p className="single-product-name" id="name">{product.name}<br />{product.volume} ml</p>
                         <div className="price">
-                            <p className="pricetag" id="price"></p>
+                            <p className="pricetag" id="price">{product.price}</p>
                             <p className="vat">incl. VAT</p>
                         </div>
-                        <p className="product-brand" id="brand">Made by HomePond Inc.</p>
-                        <p className="product-description hide-text" id="description">Lorem ipsum dolor sit amet,
-                            consectetur
-                            adipiscing
-                            elit, sed do
-                            eiusmod
+                        <p className="product-brand" id="brand">{product.brand}</p>
+                        <p className="product-description hide-text" id="description">{product.description}
                         </p>
                     </div>
                     <div className="purchase details">
                         <div className="delivery-details">
-                            <p>Free delivery to:</p>
+                            <p>Free delivery to: {user.city}</p>
                             <p className="address" id="address">
                             </p>
                         </div>
