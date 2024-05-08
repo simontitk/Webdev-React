cb2360fd0a75692754883814a9556373e125b556
simontitk <simontitk@gmail.com>
2024-05-08 13:33:55 +0200

    add link to category-cards

--- src/components/home/CategoryCard.tsx
+++ src/components/home/CategoryCard.tsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom"
 
 interface CategoryCardProps {
+    id: number,
     name: string,
     description: string
 }
 
-export default function CategoryCard({name, description}: CategoryCardProps) {
+export default function CategoryCard({id, name, description}: CategoryCardProps) {
 
     return (
-        <div className={`category-card`} style={{
+        <Link to={`/all_products?category=${id}`} className={`category-card`} style={{
                 background: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)), url("http://localhost:3000/images/categories/${name}.jpg")`,
                 backgroundPosition: "center center"
         }}>
@@ -18,6 +19,6 @@ export default function CategoryCard({name, description}: CategoryCardProps) {
             <div className="category-description"> 
                 {description} 
             </div>
-        </div>
+        </Link>
     );
 }
