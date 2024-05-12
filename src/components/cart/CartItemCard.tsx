82863f047dd6dee6fcd5117ec9ed020b0417f2f4
simontitk <simontitk@gmail.com>
2024-05-12 23:05:46 +0200

    restructure code

--- src/components/cart/CartItemCard.tsx
+++ src/components/cart/CartItemCard.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { CartContext } from "../../GlobalContext";
+import { removeCartItem, updateCartItem } from "../../services/cartItemService";
 
 interface CartItemCardProps {
     quantity: number,
@@ -13,45 +14,35 @@ interface CartItemCardProps {
     price: number
 }
 
-export default function CartItemCard({
-    quantity, uid, pid, name, description, picture_uri,
-    volume, amount, price}: CartItemCardProps) {
+export default function CartItemCard({quantity, uid, pid, name, description, picture_uri, volume, amount, price}: CartItemCardProps) {
+
         const { cart, setCart } = useContext(CartContext);
 
-        function changeQuantity(event: any) {
-            const newQuantity = parseInt(event.target.value)
-
-            fetch(`http://localhost:3000/cart_items/${uid}`, {
-            method: "PUT",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ "pid": pid, "quantity": newQuantity })
-        })
-            .then(res => res.json())
-            .then(data => {
-                const updatedItem = data.item
-                console.log(updatedItem)
-                setCart([...cart.filter(i => i.pid !== pid), updatedItem])
-            })
-            .catch(err => console.log(err));
-        };
-
-        return(
+        return (
             <div>
                 <div className="basket-item-left-container" id="{id}"> 
-                    <img className="basket-item-image" width={65} height={65} src={`http://localhost:3000/images/products/${picture_uri}`}/>
+                    <img className="basket-item-image" width={65} height={65} src={`http://localhost:3000/images/products/${picture_uri}`} alt={description}/>
                     <div className="basket-item-name-container">
                 <span className="basket-item-title">{name}</span>
                 <span className="basket-item-size">{amount} x {volume} ml</span>
                 <div className="input-group quantity-selector">
-                    <input type="number" id="inputQuantitySelector" className="form-control" 
-                        name="quantity" title="quantity" value={quantity} min="0" max="99" step="1" onChange={changeQuantity}/>
+                    <input 
+                        type="number" 
+                        id="inputQuantitySelector" 
+                        className="form-control" 
+                        name="quantity" 
+                        title="quantity" 
+                        value={quantity} 
+                        min="1" 
+                        max="99" 
+                        step="1" onChange={(e) => updateCartItem(uid, pid, parseInt(e.target.value), cart, setCart)}/>
                         </div>
                     </div>
                 </div>
             <div className="basket-item-space"></div>
             <div className="basket-item-price-container">
                 <span className="basket-item-price">{price}</span>
-                <button className="button-delete" role="button">delete</button>
+                <button className="button-delete" role="button" onClick={() => removeCartItem(uid, pid, cart, setCart)}>delete</button>
             </div>
             </div>
         )
