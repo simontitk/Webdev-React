8b8c4a746ceb310da28ffa57a245006005ab4a5d
simontitk <simontitk@gmail.com>
2024-05-14 14:37:14 +0200

    add more cart functionalities

--- src/services/cartItemService.tsx
+++ src/services/cartItemService.tsx
@@ -14,17 +14,24 @@ export function addCartItem(uid: number, pid: number, quantity: number, cart: Ca
 
 
 export function removeCartItem(uid: number, pid: number, cart: CartItem[], setCart: Function) {
-
-    /**
-     * for julian to implement
-     */
-
+    fetch(`http://localhost:3000/cart_items/${uid}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ "pid": pid})
+    })
+    .then(res => res.json())
+    .then(() => setCart([...cart.filter(i => i.pid !== pid)]))
+    .catch(err => console.log(err));
 }
-
-
+ 
+ 
 export function updateCartItem(uid: number, pid: number, quantity: number, cart: CartItem[], setCart: Function) {
-
-    /**
-     * for julian to implement
-     */
+    fetch(`http://localhost:3000/cart_items/${uid}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ "pid": pid, "quantity": quantity })
+    })
+    .then(res => res.json())
+    .then(data => setCart([...cart.filter(i => i.pid !== pid), data.item]))
+    .catch(err => console.log(err));
 }
